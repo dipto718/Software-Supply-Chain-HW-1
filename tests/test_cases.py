7354dda21969b6cacebb8c5a76a72416532444d6
dipto718 <ssd410@nyu.edu>
2025-11-10 23:53:47 -0500

    added testing and coverage for part 5

---
+++ tests/test_cases.py
@@ -0,0 +1,268 @@
+import sys
+import json
+from jsonschema import validate
+import subprocess
+import os
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(curr_dir)
+sys.path.append(parent_dir)
+
+import main
+import merkle_proof
+import util
+
+checkpoint = {
+	"type": "object",
+	"properties": {
+		"inactiveShards": {"type": "array"},
+	    "rootHash": {"type": "string"},
+	    "signedTreeHead": {"type": "string"},
+	    "treeID": {"type": "string"},
+	    "treeSize": {"type": "integer"}
+    },
+    "required": ["inactiveShards", "rootHash", "signedTreeHead", "treeID", "treeSize"]
+}
+
+# case 0
+# the example test that was given
+# doesn't seem to count
+# in the coverage report for main.py
+# tests -c
+def test_checkpoint():
+	result = subprocess.run(
+		['python3', 'main.py', '-c'],
+		capture_output = True,
+		text = True
+	)
+
+	output = result.stdout
+	data = json.loads(output)
+
+	validate(instance=data, schema=checkpoint)
+
+# case 1
+# tests that inclusion works
+# with these valid inputs and
+# that no exception is raised
+# def test_inclusion():
+# 	try:
+# 		result = subprocess.run(
+# 			['python3', 'main.py', '--inclusion 547323620', '--artifact', 'artifact.md'],
+# 			capture_output = True,
+# 			text = True
+# 		)
+# 		assert True
+# 	except:
+# 		assert False
+
+# case 2
+# tests that consistency works
+# with these valid inputs and
+# that no exception is raised
+# def test_consistency():
+# 	try:
+# 		result = subprocess.run(
+# 			['python3', 'main.py', '--consistency', '--tree-id 1193050959916656506', '--tree-size 566822630', '--root-hash', '6e9b436995ed0978ea0acc8d86dc8375c08c7c0e2c8e62cc5fe4285fe63a024f'],
+# 			capture_output = True,
+# 			text = True
+# 		)
+# 		assert True
+# 	except:
+# 		assert False
+
+# case 3
+# tests that consistency runs
+# with invalid inputs and
+# prints an error message rather
+# than crashing
+# def test_consistency_invalid():
+# 	try:
+# 		result = subprocess.run(
+# 			['python3', 'main.py', '--consistency', '--tree-id 2193050959916656506', '--tree-size 566822630', '--root-hash', '6e9b436995ed0978ea0acc8d86dc8375c08c7c0e2c8e62cc5fe4285fe63a024f'],
+# 			capture_output = True,
+# 			text = True
+# 		)
+# 		assert True
+# 	except:
+# 		assert False
+
+# case 4
+# tests that consistency runs
+# with invalid inputs and
+# prints an error message rather
+# than crashing
+# def test_inclusion_invalid():
+# 	try:
+# 		result = subprocess.run(
+# 			['python3', 'main.py', '--inclusion 54', '--artifact', 'artifact.md'],
+# 			capture_output = True,
+# 			text = True
+# 		)
+# 		assert True
+# 	except:
+# 		assert False
+
+# case 5
+# makes sure that it work with a valid
+# log index
+# def test_get_log_entry():
+# 	try:
+# 		entry = main.get_log_entry(547323620)
+# 		assert True
+# 	except:
+# 		assert False
+
+# case 6
+# makes sure that main itself still runs and compiles
+# even if no commands are entered with it
+# def test_main():
+# 	try:
+# 		result = subprocess.run(
+# 			['python3', 'main.py'],
+# 			capture_output = True,
+# 			text = True
+# 		)
+# 		assert True
+# 	except:
+# 		assert False
+
+#case 7
+
+
+# case 1
+# tests that getting the latest
+# checkpoint works
+def test_checkpoint_by_function():
+	result = main.get_latest_checkpoint()
+	validate(instance=result, schema=checkpoint)
+
+# case 2
+# main should sucessfully exit with an error code
+# as argumentparser gets no args to parse
+# if main() is called directly rather than adding
+# the args on the command line with python main.py [commands]
+def test_main():
+	try:
+		main.main()
+		assert False
+	except:
+		assert True
+
+# case 3
+# makes sure that the 
+# consistency check works with
+# a valid input
+# if it works, it should
+# not raise any exception
+def test_consistency():
+	prev_checkpoint = {}
+	prev_checkpoint["treeID"] = 1193050959916656506
+	prev_checkpoint["treeSize"] = 566822630
+	prev_checkpoint["rootHash"] = "6e9b436995ed0978ea0acc8d86dc8375c08c7c0e2c8e62cc5fe4285fe63a024f"
+	try:
+		main.consistency(prev_checkpoint)
+		assert True
+	except:
+		assert False
+
+# case 4
+# makes sure that the 
+# inclusion check works with
+# a valid input
+# if it works, it should
+# not raise any exception
+def test_inclusion():
+	try:
+		main.inclusion(547323620, "artifact.md")
+		assert True
+	except:
+		assert False
+
+# case 5
+# makes sure that the 
+# inclusion check fails with
+# an invalid input
+# it should raise an exception
+def test_inclusion_invalid_logindex():
+	try:
+		main.inclusion(54732362000000000, "artifact.md")
+		assert False
+	except:
+		assert True
+
+# case 6
+# makes sure that the 
+# consistency check fails with
+# an invalid input
+# it should raise an exception
+def test_consistency_invalid_rootHash():
+	prev_checkpoint = {}
+	prev_checkpoint["treeID"] = 1193050959916656506
+	prev_checkpoint["treeSize"] = 566822630
+	prev_checkpoint["rootHash"] = "709ghghb436995ed0978ea0acc8d86dc8375c08c7c0e2c8e62cc5fe4285fe63a024f"
+	try:
+		main.consistency(prev_checkpoint)
+		assert False
+	except:
+		assert True
+
+# case 7
+# makes sure that the 
+# consistency check fails with
+# an invalid input
+# it should raise an exception
+def test_consistency_invalid_treeSize():
+	prev_checkpoint = {}
+	prev_checkpoint["treeID"] = 1193050959916656506
+	prev_checkpoint["treeSize"] = 5668226300000000000
+	prev_checkpoint["rootHash"] = "6e9b436995ed0978ea0acc8d86dc8375c08c7c0e2c8e62cc5fe4285fe63a024f"
+	try:
+		main.consistency(prev_checkpoint)
+		assert False
+	except:
+		assert True
+
+# case 8
+# makes sure that the 
+# consistency check fails with
+# an invalid input
+# it should raise an exception
+def test_consistency_invalid_treeID():
+	prev_checkpoint = {}
+	prev_checkpoint["treeID"] = 119305095991665650600000000000
+	prev_checkpoint["treeSize"] = 566822630
+	prev_checkpoint["rootHash"] = "6e9b436995ed0978ea0acc8d86dc8375c08c7c0e2c8e62cc5fe4285fe63a024f"
+	try:
+		main.consistency(prev_checkpoint)
+		assert False
+	except:
+		assert True
+
+# case 9
+# makes sure that the 
+# inclusion check fails with
+# an invalid input
+# it should raise an exception
+def test_inclusion_invalid_unsigned_artifact():
+	try:
+		main.inclusion(547323620, "artifact.bundle")
+		assert False
+	except:
+		assert True
+
+# case 10
+# makes sure that the 
+# consistency check works with
+# a valid empty input
+# if it works, it should
+# not raise any exception
+# as it returns nothing rather than
+# raising an exception
+def test_consistency_empty_checkpoint():
+	prev_checkpoint = {}
+	try:
+		main.consistency(prev_checkpoint)
+		assert True
+	except:
+		assert False
